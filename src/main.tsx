4fc6cd436a920fc0497f8f2d0d35c5a3b3a7b184
Priyanka Gusani <priyanka.gusani@outlook.com>
2025-10-27 16:44:36 +0530

    New blog post - 27-10-2025
    some changes + blog

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import App from './App.tsx'
 import BlogListingPage from './pages/BlogListingPage.tsx'
 import BlogDetailPage from './pages/BlogDetailPage.tsx'
 import './index.css'
 
+// Track page views in Google Analytics
+const trackPageView = (path: string) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('config', 'G-NL4NXKB24B', {
+      page_path: path,
+    });
+  }
+};
+
+// Component to track route changes
+const PageViewTracker = () => {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
+
+  return null;
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <PageViewTracker />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<BlogListingPage />} />
@@ -17,3 +38,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Router>
   </React.StrictMode>,
 )
+
+// Declare gtag for TypeScript
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
